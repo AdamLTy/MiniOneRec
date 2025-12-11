6987717287e79f24e494558868f6ee9a922b95c7
root <z5658384@ad.unsw.edu.au>
2025-12-11 23:56:13 +0800

    uodate text2emb setting for 4090

--- rq/text2emb/amazon_text2emb.sh
+++ rq/text2emb/amazon_text2emb.sh
@@ -1,4 +1,4 @@
-accelerate launch --num_processes 8 amazon_text2emb.py \
+accelerate launch --num_processes 1 amazon_text2emb.py \
     --dataset Industrial_and_Scientific \
     --root ../../data/Amazon18/Industrial_and_Scientific \
-    --plm_checkpoint your_emb_model_path
+    --plm_checkpoint Qwen/Qwen2.5-0.5B-Instruct
